10951221f91d21d2837630c647a3f40eab914a42
Kaijun <flashnukers@gmail.com>
2018-01-26 19:20:14 +0100

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Navbar from './Navbar';
+
+
+const Layout = (props) => (
+    <div>
+        <Head>
+            <title>
+                Bitcoin Prices
+            </title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+        </Head>
+        <Navbar/>
+        <div className="container">
+        {props.children}
+        </div>
+        
+    </div>
+);
+
+export default Layout;
